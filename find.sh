ff1f1f3f35bce58de6d9cef939423bbd502de627
hochan222 <hochan049@gmail.com>
2021-05-20 15:09:40 +0900

    [FEAT] add content submodules

--- find.sh
+++ find.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+
+# Register content, yari repo in git local config
+git submodule init
+# Cloning content, yari repo into translated repo
+git submodule update
+
+# list files
 find files/en-us -type f > list.txt
